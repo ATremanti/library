4f0f0a9e36f3221b8209da385987aaa3e81c2c53
ATremanti <arthurtremanti@gmail.com>
2023-05-11 18:50:31 -0300

    created the dynamic append function, removed the placeholder books

--- script.js
+++ script.js
@@ -2,64 +2,68 @@ const mainContainer = document.querySelector('.main-container');
 const newbookBtn = document.querySelector('.new-book-btn');
 const closeBtn = document.querySelector('.btn-close');
 const addbookBtn = document.querySelector('.add-book-btn');
+const mainForm = document.querySelector('#main-form');
+const formTitle = document.querySelector('#book-title');
+const formAuthor = document.querySelector('#book-author');
+const formPages = document.querySelector('#book-pages');
 const overlay = document.querySelector('.modal-overlay');
 const modal = document.querySelector('.modal');
 
 let myLibrary = [];
+let i = 0;
 
-function Book (title, author, pages, read) {
+function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
 }
 
-function addBook (title, author, pages, read) {
-    myLibrary.push(new Book(title, author, pages, read));
+function addBook(formTitle, formAuthor, formPages, read) {
+    myLibrary.push(new Book(formTitle, formAuthor, formPages, read));
 }
 
-function appendBooks () {
-    for(i=0;i<myLibrary.length;i++) {
-        const bookItem = document.createElement('div');
-        bookItem.classList.add('book-item');
-        mainContainer.appendChild(bookItem);
+function appendShelf(book) {
 
-        const bookTitle = document.createElement('p');
-        bookTitle.classList.add('item-title');
-        bookItem.appendChild(bookTitle);
+    const bookItem = document.createElement('div');
+    bookItem.classList.add('book-item');
+    bookItem.setAttribute('data-shelf', myLibrary.indexOf(book));
+    mainContainer.appendChild(bookItem);
 
-        const bookAuthor = document.createElement('p');
-        bookAuthor.classList.add('item-author');
-        bookItem.appendChild(bookAuthor);
+    const bookTitle = document.createElement('p');
+    bookTitle.textContent = book.title;
+    bookTitle.classList.add('item-title');
+    bookTitle.setAttribute('data-title', myLibrary.indexOf(book));
+    bookItem.appendChild(bookTitle);
 
-        const bookPages = document.createElement('p');
-        bookPages.classList.add('item-pages');
-        bookItem.appendChild(bookPages);
+    const bookAuthor = document.createElement('p');
+    bookAuthor.textContent = book.author;
+    bookAuthor.classList.add('item-author');
+    bookAuthor.setAttribute('data-author', myLibrary.indexOf(book));
+    bookItem.appendChild(bookAuthor);
 
-        const title = myLibrary.map(a => a.title);
-        bookTitle.textContent = title[i];
-
-        const author = myLibrary.map(a => a.author);
-        bookAuthor.textContent = author[i];
-
-        const pages = myLibrary.map(a => a.pages);
-        bookPages.textContent = pages[i];
-    }
+    const bookPages = document.createElement('p');
+    bookPages.classList.add('item-pages');
+    bookPages.textContent = book.pages;
+    bookPages.setAttribute('data-pages', myLibrary.indexOf(book));
+    bookItem.appendChild(bookPages);
 }
 
-newbookBtn.addEventListener('click', function() {
+newbookBtn.addEventListener('click', function () {
     modal.classList.remove('hidden');
     overlay.classList.remove('hidden');
 })
 
-closeBtn.addEventListener('click', function() {
+closeBtn.addEventListener('click', function () {
     modal.classList.add('hidden');
     overlay.classList.add('hidden');
 })
 
-addBook('The Hobbit', 'J. R. R. Tolkien', 411, 'Read');
-addBook('Death on the Nile', 'Agatha Christie', 288, 'Read');
-addBook('The Art of War', 'Sun Tzu', 256, 'Read');
+addbookBtn.addEventListener('click', function () {
+    addBook(formTitle.value, formAuthor.value, formPages.value);
+    appendShelf(myLibrary[i]);
+    i++;
+    mainForm.reset();
+})
 
-appendBooks()
 console.log(myLibrary)
